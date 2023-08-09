c5a0e7beffddaf64c4196d3aaf3eba95319c9438
Dominduchami <dominikkobinski314@gmail.com>
2023-08-09 18:47:23 +0200

    Memory: Import WP map

--- MSM8909Pkg/Include/Configuration/DeviceMemoryMap.h
+++ MSM8909Pkg/Include/Configuration/DeviceMemoryMap.h
@@ -66,33 +66,43 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
                                                           ResourceType          MemoryType */
 
     /* DDR Regions */
-    {"Peripherals",       0x00000000, 0x80000000, AddMem, MEM_RES, UNCACHEABLE,  RtCode, NS_DEVICE}, // todo?
-
-    {"UEFI FD",           0x80080000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP,  BsCode, WRITE_BACK},
-    
-    {"MPPark Code",       0x80300000, 0x00040000, AddMem, MEM_RES, UNCACHEABLE,  RtCode, UNCACHED_UNBUFFERED},
-    {"DBG2",              0x80381000, 0x00004000, AddMem, SYS_MEM, SYS_MEM_CAP,  LdData, UNCACHED_UNBUFFERED},
-   // {"UEFI Info Block",   0x80389000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP,  RtData, UNCACHED_UNBUFFERED},
-    //{"Reset Data",        0x8038a000, 0x00004000, AddMem, SYS_MEM, SYS_MEM_CAP,  RtData, UNCACHED_UNBUFFERED},
-    {"Reser. Uncached0",  0x8038e000, 0x00072000, AddMem, SYS_MEM, SYS_MEM_CAP,  BsData, UNCACHED_UNBUFFERED},
+    {"HLOS 0",            0x80000000, 0x00010000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+    {"DBI Dump",          0x80010000, 0x00014000, NoHob,  MMAP_IO, INITIALIZED, Conv,   NS_DEVICE},
+    {"HLOS 1",            0x80024000, 0x001DC000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+    {"UEFI FD",           0x80200000, 0x00100000, AddMem, SYS_MEM, SYS_MEM_CAP, BsCode, WRITE_BACK},
+    {"MPPark Code",       0x80300000, 0x00040000, AddMem, MEM_RES, UNCACHEABLE, RtCode, UNCACHED_UNBUFFERED},
+    {"HLOS 2",            0x80340000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, UNCACHED_UNBUFFERED},
+    {"FBPT Payload",      0x80380000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, UNCACHED_UNBUFFERED},
+    {"DBG2",              0x80381000, 0x00004000, AddMem, SYS_MEM, SYS_MEM_CAP, LdData, UNCACHED_UNBUFFERED},
+    {"Capsule Header",    0x80385000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, UNCACHED_UNBUFFERED},
+    {"TPM Control Area",  0x80386000, 0x00003000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, UNCACHED_UNBUFFERED},
+    {"UEFI Info Block",   0x80389000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, UNCACHED_UNBUFFERED},
+    {"Reset Data",        0x8038A000, 0x00004000, AddMem, SYS_MEM, SYS_MEM_CAP, RtData, UNCACHED_UNBUFFERED},
+    {"Reser. Uncached0",  0x8038E000, 0x00072000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, UNCACHED_UNBUFFERED},
+    {"Display Reserved",  0x80400000, 0x00800000, AddMem, MEM_RES, WRITE_THROUGH, MaxMem, WRITE_THROUGH},
     {"UEFI Stack",        0x80C00000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
     {"CPU Vectors",       0x80C40000, 0x00010000, AddMem, SYS_MEM, SYS_MEM_CAP, BsCode, WRITE_BACK},
     {"Reser. Cached 0",   0x80C50000, 0x000B0000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
-    {"TZ Apps",           0x85A00000, 0x00500000, AddMem, MEM_RES, SYS_MEM_CAP,  Reserv, NS_DEVICE},
-    {"SMEM",              0x86300000, 0x00100000, AddMem, MEM_RES, UNCACHEABLE,  Reserv, UNCACHED_UNBUFFERED},
-    {"TZ/HYP",            0x86400000, 0x00280000, AddMem, SYS_MEM, SYS_MEM_CAP,  Reserv, NS_DEVICE},
- 
-    {"Display Reserved",  0x8e000000, 0x00800000, AddMem, MEM_RES, WRITE_THROUGH,MaxMem, WRITE_THROUGH},
-
-    /* RAM partition regions */
-    {"RAM Partition",     0x8cb00000, 0x01500000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK_XN},
-    /* Avoid Display Reserved */
-    {"RAM Partition",     0x8e800000, 0x11700000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK_XN},
-    /* carveout */
-    {"RAM Partition",     0xa0000000, 0x10000000, AddMem, SYS_MEM, SYS_MEM_CAP,  Conv,   WRITE_BACK_XN},
-    /* carveout */
-    {"RAM Partition",     0xc0000000, 0x30000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-
+    {"HLOS 3",            0x80D00000, 0x03300000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+    { "HLOS 4",           0x84000000, 0x02000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+    {"TZ Apps",           0x86000000, 0x00300000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
+    {"SMEM",              0x86300000, 0x00100000, AddMem, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED},
+    {"TZ/HYP",            0x86400000, 0x00280000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
+    {"HLOS 5",            0x86680000, 0x00080000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+    {"MPSS/EFS/DHMS/PIL", 0x86700000, 0x06C00000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
+    {"HLOS 6",            0x8D300000, 0xFFFFFFFF, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
+    /*--------------------- Other Regions ----------------------*/
+    {"IMEM Boot Base",    0x08600000, 0x00020000, NoHob,  SYS_MEM, INITIALIZED, Conv,   NS_DEVICE},
+    {"IMEM Cookie Base",  0x08600000, 0x00001000, AddMem, MMAP_IO, INITIALIZED, Conv,   NS_DEVICE},
+    /*--------------------- Register Regions -------------------*/
+    {"REG 1",             0x00001000,  0x00070000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 2",             0x00300000,  0x00200000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 3",             0x00700000,  0x00D00000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 4",             0x01400000,  0x00C00000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 5",             0x02000000,  0x01C00000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 6",             0x07800000,  0x00100000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 7",             0x0B000000,  0x00800000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
+    {"REG 8",             0x0C428000,  0x00008000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
 
     /* Terminator for MMU */
     {"Terminator", 0, 0, 0, 0, 0, 0, 0}};
