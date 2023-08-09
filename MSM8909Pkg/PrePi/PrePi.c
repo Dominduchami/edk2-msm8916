193a0e3395fed86991d0571a3cef4a73754d8135
Dominduchami <dominikkobinski314@gmail.com>
2023-08-09 22:32:52 +0200

    PrePi: Dynamic width for config

--- MSM8909Pkg/PrePi/PrePi.c
+++ MSM8909Pkg/PrePi/PrePi.c
@@ -101,7 +101,7 @@ PrePiMain (
   /* Switch to 32bpp argb8888 */
   MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_FORMAT, 0x000236FF);
   MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_UNPACK_PATTERN, 0x03020001);
-  MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, 720 * 4);
+  MmioWrite32(PIPE_BASE + PIPE_SSPP_SRC_YSTRIDE, FixedPcdGet32(PcdMipiFrameBufferWidth) * 4);
   MmioWrite32(MDP_CTL_0_BASE + CTL_FLUSH, (1 << (3)));
 
   // Initialize the architecture specific bits
